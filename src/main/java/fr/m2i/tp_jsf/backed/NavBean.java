95631c9bc21fb6689f82a20e3467e124bb829a5a
Olivier-Argaud-M2I <Olivier.ARGAUD@labom2iformation.fr>
2022-08-23 11:19:21 +0200

    second commit

--- src/main/java/fr/m2i/tp_jsf/backed/NavBean.java
+++ src/main/java/fr/m2i/tp_jsf/backed/NavBean.java
@@ -1,6 +1,11 @@
 package fr.m2i.tp_jsf.backed;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.annotation.ManagedProperty;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
@@ -12,7 +17,21 @@ public class NavBean implements Serializable
 {
 	
 	private static final long serialVersionUID = 1L;
-	
+
+
+	@Inject
+	@ManagedProperty(value = "#{param.pageId}")
+	private String pageId;
+
+	@PostConstruct
+	public void post(){
+		System.out.println("apres creation du bean");
+	}
+
+	@PreDestroy
+	public void pre(){
+		System.out.println("avant la destruction du bean");
+	}
 	
 	public String index() {
 		return "index?faces-redirect=true";
@@ -24,5 +43,11 @@ public class NavBean implements Serializable
 
 	public String login() {
 		return "login?faces-redirect=true";
+	}
+
+
+	public String page(){
+		System.out.println(pageId);
+		return this.pageId+"?faces-redirect=true";
 	}
 }
--- src/main/java/fr/m2i/tp_jsf/backed/UserBean.java
+++ src/main/java/fr/m2i/tp_jsf/backed/UserBean.java
@@ -1,6 +1,10 @@
 package fr.m2i.tp_jsf.backed;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.annotation.ManagedProperty;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
@@ -13,12 +17,11 @@ public class UserBean implements Serializable
 {
 	
 	private static final long serialVersionUID = 1L;
-	
+
+
 	private String nom = "argaud";
 	private String prenom = "olivier";
-
 	private String password = "";
-
 	private String username;
 
 	public String getNom() {
@@ -27,27 +30,21 @@ public class UserBean implements Serializable
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
 	public String getPrenom() {
 		return prenom;
 	}
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
